ef800e59cee7c283d28f61e9f56c4319e5a253d6
“[bridgettenawari]” <[chebetbri@gmail.com]>
2026-07-04 22:33:08 +0300

    done

--- tests/test_app.py
+++ tests/test_app.py
@@ -6,7 +6,7 @@ import pytest
 # Ensure the root project directory is on the path
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-from app import app
+from server import app
 
 @pytest.fixture
 def client():
